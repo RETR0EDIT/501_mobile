7ffa6c958e1da1f690a980ab64e6867abb476ea3
RETRO EDIT <nolandme2003@gmail.com>
2025-03-05 11:46:28 +0100

    connexion

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Text, View } from "@/components/Themed";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>HOme</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
